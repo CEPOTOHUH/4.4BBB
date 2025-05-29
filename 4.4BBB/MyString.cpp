c19f61a18345feb675ba8fc4b496f7f693830fa8
CEPOTOHUH <srocnozajdi@gmail.com>
2025-05-29 22:38:11 +0300

    Update MyString.cpp

--- 4.4BBB/MyString.cpp
+++ 4.4BBB/MyString.cpp
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "MyString.h"
 #include <cstring>
+#include <utility>
 
 bool MyString::isSpace(char c) const {
     return (c == ' ');
@@ -9,8 +10,7 @@ bool MyString::isSpace(char c) const {
 void MyString::copyFrom(const MyString& other) {
     length = other.length;
     data = new char[length + 1];
-    for (size_t i = 0; i <= length; ++i)
-        data[i] = other.data[i];
+    std::strcpy(data, other.data);
 }
 
 void MyString::moveFrom(MyString&& other) {
@@ -29,6 +29,11 @@ void MyString::free() {
 MyString::MyString() : data(new char[1] {'\0'}), length(0) {}
 
 MyString::MyString(const char* str) {
+    if (!str) {
+        data = new char[1] {'\0'};
+        length = 0;
+        return;
+    }
     length = std::strlen(str);
     data = new char[length + 1];
     std::strcpy(data, str);
@@ -38,7 +43,7 @@ MyString::MyString(const MyString& other) {
     copyFrom(other);
 }
 
-MyString::MyString(MyString&& other) {
+MyString::MyString(MyString&& other) noexcept {
     moveFrom(std::move(other));
 }
 
@@ -50,9 +55,9 @@ MyString& MyString::operator=(const MyString& other) {
     return *this;
 }
 
-MyString& MyString::operator=(MyString&& other) {
+MyString& MyString::operator=(MyString&& other) noexcept {
     if (this != &other) {
-        //free();
+        free();
         moveFrom(std::move(other));
     }
     return *this;
@@ -66,24 +71,39 @@ MyString& MyString::operator+=(std::pair<char, size_t> p) {
     char c = p.first;
     size_t pos = p.second;
 
-    size_t newLength = (pos > length) ? pos + 1 : length + 1;
+    const size_t MAX_ALLOWED_OFFSET = 1024;
+    if (pos > length + MAX_ALLOWED_OFFSET) {
+        pos = length + MAX_ALLOWED_OFFSET;
+    }
+
+    size_t newLength = (pos >= length) ? pos + 1 : length + 1;
+
+    if (newLength < pos) { 
+        newLength = pos; 
+    }
+
     char* newData = new char[newLength + 1];
 
     for (size_t i = 0; i < pos; ++i) {
-        if (i < length)
+        if (i < length) {
             newData[i] = data[i];
-        else
+        }
+        else {
             newData[i] = ' ';
+        }
     }
 
     newData[pos] = c;
 
-    for (size_t i = pos; i < length; ++i)
-        newData[i + 1] = data[i];
+    if (pos < length) {
+        for (size_t i = pos; i < length; ++i) {
+            newData[i + 1] = data[i];
+        }
+    }
 
     newData[newLength] = '\0';
 
-    delete[] data;
+    free();
     data = newData;
     length = newLength;
 
@@ -100,7 +120,7 @@ MyString& MyString::operator--() {
         for (size_t i = 0; i < newLength; ++i)
             newData[i] = data[i + index];
         newData[newLength] = '\0';
-        delete[] data;
+        free();
         data = newData;
         length = newLength;
     }
@@ -108,35 +128,48 @@ MyString& MyString::operator--() {
 }
 
 MyString MyString::operator+(const MyString& other) const {
-
-    int sizemax = this->length > other.length ? this->length : other.length;
-    int sizemin = this->length < other.length ? this->length : other.length;
-    char* newstr = new char[sizemax];
-
-    for (int i = 0;i < sizemin;i++) {
-        newstr[i] = other.data[i];
+    size_t resultLength;
+    if (this->length > other.length) {
+        resultLength = this->length;
+    }
+    else {
+        resultLength = other.length;
     }
 
-    for (int i = sizemin;i < sizemax;i++)
-        newstr[i] = data[i];
-  
-    newstr[sizemax] = '\0';
+    char* newStrData = new char[resultLength + 1];
+    for (size_t i = 0; i < this->length; ++i) {
+        newStrData[i] = this->data[i];
+    }
+    for (size_t i = this->length; i < resultLength; ++i) {
+        newStrData[i] = '\0'; 
+    }
+    for (size_t i = 0; i < other.length; ++i) {
+        newStrData[i] = other.data[i];
+    }
 
-    MyString newmy(newstr);
+    newStrData[resultLength] = '\0'; 
 
-    return newmy;
+    MyString result(newStrData);
+    delete[] newStrData;
+
+    return result;
 }
 
-
 std::ostream& operator<<(std::ostream& out, const MyString& str) {
-    out << str.data;
+    if (str.data) {
+        out << str.data;
+    }
+    else {
+        out << "";
+    }
     return out;
 }
 
 std::istream& operator>>(std::istream& in, MyString& str) {
-    char buffer[1024];
-    in.ignore(10000, '\n');
-    in.getline(buffer, 1024);
+    char buffer[8192];
+    in.getline(buffer, sizeof(buffer));
     str = MyString(buffer);
+    return in;
+}
     return in;
 }
