8bf8d44372474a3cfb7c87a1e3c44be4887a0610
CEPOTOHUH <srocnozajdi@gmail.com>
2025-05-29 22:38:28 +0300

    Update MyString.h

--- 4.4BBB/MyString.h
+++ 4.4BBB/MyString.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <utility>
 
 class MyString {
 private:
@@ -12,19 +13,18 @@ private:
     void free();
 
 public:
-    MyString();                              // Конструктор по умолчанию
-    MyString(const char* str);              // Конструктор с параметром
-    MyString(const MyString& other);        // Копирующий конструктор
-    MyString(MyString&& other);             // Перемещающий конструктор
-    MyString& operator=(const MyString& other); // Оператор копирующего присваивания
-    MyString& operator=(MyString&& other);      // Оператор перемещающего присваивания
-    ~MyString();                            // Деструктор
+    MyString();
+    MyString(const char* str);
+    MyString(const MyString& other);
+    MyString(MyString&& other) noexcept;
+    MyString& operator=(const MyString& other);
+    MyString& operator=(MyString&& other) noexcept;
+    ~MyString();
 
-    MyString& operator+=(std::pair<char, size_t> p);  // Вставка символа
-    MyString& operator--();                          // Удаление ведущих пробелов
-    MyString operator+(const MyString& other) const;  // Сложение строк
+    MyString& operator+=(std::pair<char, size_t> p);
+    MyString& operator--();
+    MyString operator+(const MyString& other) const;
 
     friend std::ostream& operator<<(std::ostream& out, const MyString& str);
     friend std::istream& operator>>(std::istream& in, MyString& str);
 };
-
